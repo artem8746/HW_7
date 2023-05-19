d969579408ef39cfe87588007dbf6fdf2d8abb58
Artem Malikov <artemmalikov742@gmail.com>
2023-05-19 22:46:10 +0300

    Add project files.

---
+++ Controllers/ToDoListController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoListWebAPI_HW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ToDoListController : ControllerBase
+    {
+        public static List<Target> targets = new List<Target>() { new Target { Id = 12, TargetValue = "Wake up" } };
+        //список с задачами
+
+        [HttpGet]
+        public ActionResult GetTargets() 
+        {
+            if(targets != null) 
+            {
+                return Ok(targets);
+            }
+
+            return StatusCode(404);
+        }
+        //ендпоинт для получения всех задач
+
+        [HttpPost]
+        public ActionResult AddTarget([FromBody] CreateTargetRequest request)
+        {
+            var target = new Target
+            {
+                Id = targets[targets.Count() - 1].Id + 1,
+                TargetValue = request.TargetValue,
+                Description = request.Description,
+                IsCompleted = false
+            };
+
+            targets.Add(target);
+
+            return Ok(target);
+        }
+        //ендпоинт для добавления задач
+
+        [HttpPatch("{id}")]
+        public ActionResult ChangeTarget([FromRoute] int id, [FromBody] ChangeTargetRequest request)
+        {            
+            var target = targets.FirstOrDefault(x => x.Id == id);
+            if(target != null)
+            {
+                target.TargetValue = request.NewTargetValue;
+                target.IsCompleted = request.IsCompleted;
+                target.Description = request.Description;
+                
+                return Ok(target);
+            }    
+            else
+                return BadRequest(request);
+        }
+        //ендпоинт для изменения задач по id
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteTarget([FromRoute] int id)
+        {
+            var target = targets.FirstOrDefault(x => x.Id == id);
+            if (target != null)
+            {
+                Ok(targets.Remove(target));
+                return Ok(target);
+            }
+            else
+                return BadRequest();
+        }
+        //ендпоинт для удаления задач по id
+
+        [HttpGet("{id}")]
+        public ActionResult GetTarget([FromRoute] int id)
+        {
+            var target = targets.FirstOrDefault(x => x.Id == id);
+
+            if(target != null)
+                return Ok(target);
+
+            return BadRequest();
+        }
+        //ендпоинт для получения одной задачи по id
+    }
+
+    public class ChangeTargetRequest
+    {
+        public string NewTargetValue { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+    //класс для создания объекта запроса на изменение задачи
+
+    public class CreateTargetRequest
+    {
+        public string TargetValue { get; set; }
+
+        public string Description { get; set; }
+    }
+    //класс для создания объекта запроса на создание задачи
+}
---
+++ Target.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoListWebAPI_HW
+{
+    public class Target
+    {
+        public int Id { get; set; }
+
+        public string TargetValue { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
